5f6f42e673190496431b7fc1ff3a401e5f7c8162
Praveen Posa S <srinivaspraveen043@gmail.com>
2026-08-19 13:44:39 +0000

    Feature Division Function

--- Bash/calculator.sh
+++ Bash/calculator.sh
@@ -15,12 +15,17 @@ multiply(){
     echo "Product = $result"
 }
 
+divide(){
+    result=$((num1 / num2))
+    echo "Answer = $result"
+}
 echo "Simple Calculator"
 echo "1. Addition"
 echo "2. Subtraction"
 echo "3. Multiplication"
+echo "4. division"
 
-read -p "Choose an option (1 / 2 / 3): " choice
+read -p "Choose an option (1 / 2 / 3 / 4): " choice
 
 read -p "Enter first number: " num1
 read -p "Enter second number: " num2
@@ -38,6 +43,10 @@ case $choice in
         multiply
         ;;
 
+    4) 
+        divide
+        ;;
+        
     *)
         echo "Invalid choice!"
         ;;
