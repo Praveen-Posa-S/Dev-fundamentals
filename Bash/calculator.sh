8748417bc4674f895a3a2db4034b538171e1eafc
Praveen Posa S <srinivaspraveen043@gmail.com>
2026-08-19 13:37:12 +0000

    feature Multiplication

--- Bash/calculator.sh
+++ Bash/calculator.sh
@@ -10,14 +10,21 @@ subtract() {
     echo "Difference = $result"
 }
 
+multiply(){
+    result=$((num1 * num2))
+    echo "Product = $result"
+}
+
 echo "Simple Calculator"
 echo "1. Addition"
 echo "2. Subtraction"
+echo "3. Multiplication"
 
-read -p "Choose an option (1 or 2): " choice
+read -p "Choose an option (1 / 2 / 3): " choice
 
 read -p "Enter first number: " num1
 read -p "Enter second number: " num2
+
 
 case $choice in
     1)
@@ -26,6 +33,11 @@ case $choice in
     2)
         subtract
         ;;
+    
+    3)
+        multiply
+        ;;
+
     *)
         echo "Invalid choice!"
         ;;
